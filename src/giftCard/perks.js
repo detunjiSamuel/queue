7edb84c381f43bfb77df6a78e8cf05da8ad81d62
Adetunji Samuel <samadetunji01@gmail.com>
2022-09-12 04:19:58 +0100

    begin gift-cards

---
+++ src/giftCard/perks.js
@@ -0,0 +1,217 @@
+module.exports = [
+  {
+    "Name": "Netflix Basic",
+    "Cost": 8.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Netflix Standard",
+    "Cost": 13.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Netflix Premium",
+    "Cost": 17.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Hulu (No Ads)",
+    "Cost": 11.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Hulu (Standard)",
+    "Cost": 5.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Hulu + Live TV",
+    "Cost": 64.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Audible Plus",
+    "Cost": 7.95,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Audible Premium Plus",
+    "Cost": 14.95,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Masterclass",
+    "Cost": 180,
+    "Period": "Annual"
+  },
+  {
+    "Name": "Disney+",
+    "Cost": 7.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "People Magazine",
+    "Cost": 89.1,
+    "Period": "Annual"
+  },
+  {
+    "Name": "Time Magazine Digital",
+    "Cost": 19.99,
+    "Period": "Annual"
+  },
+  {
+    "Name": "Calm",
+    "Cost": 69.99,
+    "Period": "Annual"
+  },
+  {
+    "Name": "Headspace",
+    "Cost": 12.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "The Economist Digital",
+    "Cost": 55,
+    "Period": "Quarterly"
+  },
+  {
+    "Name": "The Economist Digital + Print",
+    "Cost": 69,
+    "Period": "Quarterly"
+  },
+  {
+    "Name": "HBOMAX (with Ads)",
+    "Cost": 9.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "HBOMAX (Ad-free)",
+    "Cost": 14.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Discovery+ (with Ads)",
+    "Cost": 4.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Discovery+ (Ad-free)",
+    "Cost": 6.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Hulu, Disney+ and ESPN+ Bundle",
+    "Cost": 13.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "ClassPass (6 credits or 2 classes)",
+    "Cost": 15,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "ClassPass (23 credits or 8 classes)",
+    "Cost": 49,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "ClassPass (38 credits or 13 classes)",
+    "Cost": 79,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "ClassPass (68 credits or 24 classes)",
+    "Cost": 139,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "ClassPass (100 credits or 35 classes)",
+    "Cost": 199,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Apple Music (Individual)",
+    "Cost": 9.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Apple Music (Family)",
+    "Cost": 14.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Babbel",
+    "Cost": 13.95,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Spotify Individual",
+    "Cost": 9.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Spotify Duo; For 2 People Living Together",
+    "Cost": 12.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Spotify Family; Up to 6 Accounts",
+    "Cost": 15.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Spotify Student; Incluldes Hulu (with Ads) and SHOWTIME",
+    "Cost": 4.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Amazon Music Unlimited",
+    "Cost": 7.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Amazon Music Family",
+    "Cost": 14.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "SHOWTIME",
+    "Cost": 10.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "The Wall Street Journal",
+    "Cost": 38.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "The New York Times Basic Digital",
+    "Cost": 19.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "The New York Times Basic Digital, NYT Games, NYT Cooking",
+    "Cost": 23.95,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "The Washinton Post All-Access Digital",
+    "Cost": 11.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "The Washinton Post Shared Access",
+    "Cost": 17.99,
+    "Period": "Monthly"
+  },
+  {
+    "Name": "Rosetta Stone",
+    "Cost": 36,
+    "Period": "Quarterly"
+  },
+  {
+    "Name": "Duolingo Plus",
+    "Cost": 6.99,
+    "Period": "Monthly"
+  }
+]
