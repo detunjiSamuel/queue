ad0404030796ab978af1e886910280f5e00969e1
Adetunji Samuel <samadetunji01@gmail.com>
2022-10-19 10:46:54 +0100

    fix: remove repeated cron

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -6,10 +6,10 @@ const registerJobs = () => {
   cron.schedule('0 8 * * *', async () => {
     await processAutosaving('daily');
   });
-  cron.schedule('0 8 * * *', async () => {
+  cron.schedule('0 0 * * 0', async () => {
     await processAutosaving('weekly');
   });
-  cron.schedule('0 8 * * *', async () => {
+  cron.schedule('0 0 1 * *', async () => {
     await processAutosaving('monthly');
   });
 };
