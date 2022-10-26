5c930aab9fc715d2c339c4b6e9afa4620f8b253d
Adetunji Samuel <samadetunji01@gmail.com>
2022-10-26 20:06:30 +0100

    modified:   package.json 	modified:   seed/extractor.ts 	modified:   seed/seeder.ts 	modified:   test/binance.test.ts

--- seed/extractor.ts
+++ seed/extractor.ts
@@ -1,44 +1,41 @@
-import Card from '../src/models/card.model'
-import CoinRequest from '../src/models/coinRequest.model'
-import EmailVerification from '../src/models/emailVerification.model'
-import Transaction from '../src/models/transaction.model'
-import User from '../src/models/user.model'
-import Savings from '../src/models/savings.model'
+import Card from '../src/models/card.model';
+import CoinRequest from '../src/models/coinRequest.model';
+import EmailVerification from '../src/models/emailVerification.model';
+import Transaction from '../src/models/transaction.model';
+import User from '../src/models/user.model';
+import Savings from '../src/models/savings.model';
 
-import startDb from '../src/config/database'
+import startDb from '../src/config/database';
 
 import { writeFile } from 'fs/promises';
 
 // Generate seed data from my local DB
 async function extractor() {
-    startDb()
-    try {
-        await handleAll({
-            User,
-            Card,
-            Transaction,
-            EmailVerification,
-            CoinRequest,
-            Savings
-        })
-        console.log("extration complete")
-    }
-    catch (e) {
-        console.log(e.message)
-        console.error('handle All failed')
-    }
+  startDb();
+  try {
+    await handleAll({
+      User,
+      Card,
+      Transaction,
+      EmailVerification,
+      CoinRequest,
+      Savings,
+    });
+    console.log('extration complete');
+  } catch (e) {
+    console.log(e.message);
+    console.error('handle All failed');
+  }
 }
 
 async function handleAll(models: any) {
-
-    for (const model in models) {
-        console.log(`${model} extration`)
-        const content = JSON.stringify(await models[model].find())
-        await writeFile(`./seed/data/${model}.json`, content, 'utf8')
-        console.log("The file was saved!");
-    }
-    return
-
+  for (const model in models) {
+    console.log(`${model} extration`);
+    const content = JSON.stringify(await models[model].find());
+    await writeFile(`./seed/data/${model}.json`, content, 'utf8');
+    console.log('The file was saved!');
+  }
+  return;
 }
 
-extractor()
+extractor();
--- test/binance.test.ts
+++ test/binance.test.ts
@@ -1,12 +1,13 @@
-import { validateAddress } from "../src/services/binance.service";
-import {testAddress} from '../mocks/index'
+import { describe, expect, test } from '@jest/globals';
+import { validateAddress } from '../src/services/binance.service';
+import { testAddress } from '../mocks/index';
 describe('binance tests', () => {
-    for (let address in testAddress) {
-        test('can identify network of coin', () => {
-            expect(validateAddress(testAddress[address])).toBe(address)
-        })
-    }
-    test('can identify invalid address', () => {
-        expect(validateAddress('randomstring')).toBeFalsy()
-    })
-})
+  for (const address in testAddress) {
+    test('can identify network of coin', () => {
+      expect(validateAddress(testAddress[address])).toBe(address);
+    });
+  }
+  test('can identify invalid address', () => {
+    expect(validateAddress('randomstring')).toBeFalsy();
+  });
+});
